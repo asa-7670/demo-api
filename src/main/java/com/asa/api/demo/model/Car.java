52d90f82cf5e56179d081c58be2fe29beb36d354
abdelhalim.saidi <abdelhalim.saidi@sis-marches.com>
2020-07-27 15:54:43 +0200

    improve car model add categoryLabel, markLabel and energyLabel update data.sql

--- src/main/java/com/asa/api/demo/model/Car.java
+++ src/main/java/com/asa/api/demo/model/Car.java
@@ -1,13 +1,15 @@
 package com.asa.api.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.asa.api.demo.constant.car.Category;
+import com.asa.api.demo.constant.car.Energy;
+import com.asa.api.demo.constant.car.Mark;
+import lombok.*;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -20,10 +22,10 @@ public class Car {
     @Id
     @Column(name = "CAR_ID", length = 36, nullable = false, insertable = false, updatable = false)
     private String id;
-    @Column(name = "CAR_CATEGORY", length = 25, nullable = false)
+    @Column(name = "CAR_CATEGORY", length = 2, nullable = false)
     @NotEmpty
     private String category;
-    @Column(name = "CAR_MARK", length = 25, nullable = false)
+    @Column(name = "CAR_MARK", length = 3, nullable = false)
     @NotEmpty
     private String mark;
     @Column(name = "CAR_MODEL", length = 25, nullable = false)
@@ -38,4 +40,21 @@ public class Car {
     @Column(name = "CAR_POWER", length = 4, nullable = false)
     @NotNull
     private Integer power;
+
+    @Transient
+    private String categoryLabel;
+    public String getCategoryLabel(){
+        return StringUtils.isEmpty(this.category) ? null : Category.of(this.category).name();
+    }
+    @Transient
+    private String markLabel;
+    public String getMarkLabel() {
+        return StringUtils.isEmpty(this.mark) ? null : Mark.of(this.mark).name();
+    }
+    @Transient
+    private String EnergyLabel;
+    public String getEnergyLabel(){
+        return StringUtils.isEmpty(this.energy) ? null : Energy.of(this.energy).name();
+    }
+
 }
